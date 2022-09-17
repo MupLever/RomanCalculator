fbff58d26981e5b51f2ef6d9e808c5452f0d868e
MupLever <90530335+MupLever@users.noreply.github.com>
2022-09-17 14:30:43 +0300

    add test roman numbers

--- Main.java
+++ Main.java
@@ -1,9 +1,25 @@
 import java.util.Scanner;
 public class Main {
+    public static boolean isRomanNumber(String string) { 
+	    String [] romanNumbers = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+	    for (int i = 0; i < 10; ++i) {
+	        if (string.equals(romanNumbers[i]))
+	            return true;
+	    }
+	    return false;
+	}
+
     public static void main(String[] args) {
         System.out.println("Введите выражение, состоящее из двух арабских или римских чисел от 1 до 10 и операции между ними.");
-		Scanner scanner = new Scanner(System.in);
-		String string = scanner.next();
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.next();
+        String[] operands = string.split("[-+/*]");
+        if (operands.length != 2) {
+            System.out.println("Ошибка! Количество операндов не равно двум.");
+        } else {
+            boolean flag1 = isRomanNumber(operands[0]);
+            boolean flag2 = isRomanNumber(operands[1]);
+        }
         return 0;
     }
 }
