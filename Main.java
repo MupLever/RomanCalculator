ef61eae50de50513003b12952b31cd2b99458b2d
MupLever <90530335+MupLever@users.noreply.github.com>
2022-09-17 15:27:45 +0300

    add two function
    add str_to_int and roman_number_to_int

--- Main.java
+++ Main.java
@@ -1,5 +1,31 @@
 import java.util.Scanner;
 public class Main {
+    public static int romanNumberToInt(String string) throws
+	Exception {
+	    String [] romanNumbers = {"I","II","III","IV","V","VI","VII","VIII","IX","X"};
+	    boolean flag = true;
+	    for (int i = 0; i < 10 && flag; i++) {
+	        if (string.equals(romanNumbers[i])) {
+	            flag = false;	            
+	            return ++i;
+	        }
+	    }
+	    if (flag)
+	    throw new
+	        Exception("Ошибка! Число либо неположительно, либо больше 10");
+	   return Integer.parseInt(string);
+	}
+
+    public static int StrToIntNumber(String string) throws
+	Exception {
+	    int number = Integer.parseInt(string);
+	    if (number < 0 || number > 10)
+	    throw new
+	        Exception("Ошибка! Число либо неположительно, либо больше 10");
+	    else
+			return number;
+	}
+
     public static boolean isRomanNumber(String string) { 
 	    String [] romanNumbers = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
 	    for (int i = 0; i < 10; ++i) {
@@ -19,6 +45,8 @@ public class Main {
         } else {
             boolean flag1 = isRomanNumber(operands[0]);
             boolean flag2 = isRomanNumber(operands[1]);
+            int value1 = flag1 ? romanNumberToInt(operands[0]) : StrToIntNumber(operands[0]);
+            int value2 = flag2 ? romanNumberToInt(operands[1]) : StrToIntNumber(operands[1]);
         }
         return 0;
     }
